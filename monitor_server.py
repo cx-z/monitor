05aace6ecf585f4540bac7c1b8089957e3712ad3
cx-z <1612379420@qq.com>
2017-04-26 21:14:31 +0800

    monitor_server.py
    增加了登录退出函数

--- monitor_server.py
+++ monitor_server.py
@@ -1,12 +1,38 @@
-from flask import Flask, jsonify
+from flask import Flask,request,session,jsonify,url_for,redirect
 from flask.templating import render_template
 
 from storage import table
 
 app = Flask(__name__)
+app.config.from_object(__name__)
 
+# Load default config and override config from an environment variable
+app.config.update(dict(
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='qwerty'
+))
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
+def login():
+	error=None
+	if request.method=='POST':
+		if request.form['username']!=app.config['USERNAME']:
+			error='Invalid username'
+		elif request.form['password']!=app.config['PASSWORD']:
+			error='Invalid password'
+		else:
+			session['logged_in']=True
+			return redirect(url_for('index'))
+	return render_template('login.html',error=error)
+    
+@app.route('/logout')
+def logout():
+	session.pop('logged_in',None)
+	return redirect(url_for('login'))
+    
+@app.route('/display')
 def index():
     return render_template('index.html')
 
