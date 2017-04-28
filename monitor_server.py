3503d5b6be36d02eb95b14102862292b3708bb6b
cx-z <1612379420@qq.com>
2017-04-28 12:07:49 +0800

    设置了多个登录用户

--- monitor_server.py
+++ monitor_server.py
@@ -9,28 +9,29 @@ app.config.from_object(__name__)
 # Load default config and override config from an environment variable
 app.config.update(dict(
     SECRET_KEY='development key',
-    USERNAME='admin',
-    PASSWORD='qwerty'
+    #USERNAME='admin',
+    #PASSWORD='qwerty'
+    DICR_USERS={'admin1':'qwerty1','admin2':'qwerty2','admin3':'qwerty3'}
 ))
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 @app.route('/',methods=['GET','POST'])
 def login():
-	error=None
-	if request.method=='POST':
-		if request.form['username']!=app.config['USERNAME']:
-			error='Invalid username'
-		elif request.form['password']!=app.config['PASSWORD']:
-			error='Invalid password'
-		else:
-			session['logged_in']=True
-			return redirect(url_for('index'))
-	return render_template('login.html',error=error)
+    error=None
+    if request.method=='POST':
+        if not app.config['DICR_USERS'].has_key(request.form['username']):
+            error='Invalid username'
+        elif request.form['password']!=app.config['DICR_USERS'][request.form['username']]:
+            error='Invalid password'
+        else:
+            session['logged_in']=True
+            return redirect(url_for('index'))
+    return render_template('login.html',error=error)
     
 @app.route('/logout')
 def logout():
-	session.pop('logged_in',None)
-	return redirect(url_for('login'))
+    session.pop('logged_in',None)
+    return redirect(url_for('login'))
     
 @app.route('/display')
 def index():
